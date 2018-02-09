59e6f7dd93f577d47d96336dc812a7eef1176290
Алексей Елагин <a.elagin@corp.youdo.com>
2018-02-09 15:40:05 +0300

    common syslog server

--- main.go
+++ main.go
@@ -12,12 +12,12 @@ func main() {
 	server := syslog.NewServer()
 	server.SetFormat(syslog.RFC5424)
 	server.SetHandler(handler)
-	server.ListenUDP("0.0.0.0:514")
+	server.ListenUDP("0.0.0.0:5140")
 	server.Boot()
 
 	go func(channel syslog.LogPartsChannel) {
 		for logParts := range channel {
-			fmt.Println(logParts)
+			fmt.Printf("%#v", logParts)
 		}
 	}(channel)
 
