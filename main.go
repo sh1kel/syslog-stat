7683e8119c98871376313fcfe4b9d1092e9c60bb
Алексей Елагин <a.elagin@corp.youdo.com>
2018-02-09 19:10:34 +0300

    простой парсер строки с выделением id сессии

--- main.go
+++ main.go
@@ -3,9 +3,34 @@ package main
 import (
 	"fmt"
 	"gopkg.in/mcuadros/go-syslog.v2"
+	"strings"
 )
 
+type emailMessage struct {
+	From   string
+	To     string
+	Relay  string
+	Delay  string
+	Status string
+}
+
+func parseMessage(msg string) (header, payload string) {
+	split := strings.Split(msg, ":")
+	if len(split) < 2 {
+		return "", strings.TrimSpace(msg)
+	}
+	if len(split) > 2 {
+		header = split[0]
+		payload = strings.Join(split[1:], " ")
+		return strings.TrimSpace(header), strings.TrimSpace(payload)
+	}
+	header = split[0]
+	payload = split[1]
+	return strings.TrimSpace(header), strings.TrimSpace(payload)
+}
+
 func main() {
+	// sgList := make(map[string]emailMessage)
 	channel := make(syslog.LogPartsChannel)
 	handler := syslog.NewChannelHandler(channel)
 
@@ -17,7 +42,10 @@ func main() {
 
 	go func(channel syslog.LogPartsChannel) {
 		for logParts := range channel {
-			fmt.Printf("%#v", logParts)
+			//fmt.Println(logParts["message"])
+			msg := fmt.Sprint(logParts["message"])
+			h, p := parseMessage(msg)
+			fmt.Printf("Message:\n%s\n%s\n", h, p)
 		}
 	}(channel)
 
