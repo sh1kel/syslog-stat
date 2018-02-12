994288ae75745eef2a02c00a79473aacabab054a
Алексей Елагин <a.elagin@corp.youdo.com>
2018-02-12 17:02:45 +0300

    полный парсер строки, формирование объекта сообщения

--- main.go
+++ main.go
@@ -3,15 +3,18 @@ package main
 import (
 	"fmt"
 	"gopkg.in/mcuadros/go-syslog.v2"
+	"strconv"
 	"strings"
 )
 
 type emailMessage struct {
-	From   string
-	To     string
-	Relay  string
-	Delay  string
-	Status string
+	From       string
+	To         string
+	Relay      string
+	Delay      int
+	StatusCode string
+	StatusMsg  string
+	RawRecord  []string
 }
 
 func parseMessage(msg string) (header, payload string) {
@@ -29,8 +32,40 @@ func parseMessage(msg string) (header, payload string) {
 	return strings.TrimSpace(header), strings.TrimSpace(payload)
 }
 
+func (msg *emailMessage) UpdateMessage(logRecord string) {
+	// Добавляем отрпавителя
+	if strings.HasPrefix(logRecord, "from=") {
+		stringParts := strings.SplitN(logRecord, ",", 2)
+		from := strings.TrimPrefix(stringParts[0], "from=<")
+		msg.From = strings.TrimSuffix(from, ">")
+		msg.RawRecord = append(msg.RawRecord, logRecord)
+		return
+	}
+	// Добавляем получателя, релей, задержку, статус отправки
+	if strings.HasPrefix(logRecord, "to=") {
+		var err error
+		stringParts := strings.Split(logRecord, ",")
+		to := strings.TrimPrefix(stringParts[0], "to=<")
+		msg.To = strings.TrimSuffix(to, ">")
+		relay := strings.TrimPrefix(stringParts[1], "relay=")
+		msg.Relay = strings.Split(relay, "[")[0]
+		msg.Delay, err = strconv.Atoi(strings.Split(stringParts[2], "=")[1])
+		if err != nil {
+			msg.Delay = -1
+		}
+		RawStatus := stringParts[5]
+		fullStatus := strings.SplitN(RawStatus, "=", 2)[1]
+		splitStatuses := strings.SplitN(fullStatus, " ", 2)
+		msg.StatusCode = splitStatuses[0]
+		msg.StatusMsg = splitStatuses[1]
+		msg.RawRecord = append(msg.RawRecord, logRecord)
+		return
+	}
+	msg.RawRecord = append(msg.RawRecord, logRecord)
+}
+
 func main() {
-	// sgList := make(map[string]emailMessage)
+	msgList := make(map[string]emailMessage)
 	channel := make(syslog.LogPartsChannel)
 	handler := syslog.NewChannelHandler(channel)
 
@@ -42,10 +77,9 @@ func main() {
 
 	go func(channel syslog.LogPartsChannel) {
 		for logParts := range channel {
-			//fmt.Println(logParts["message"])
 			msg := fmt.Sprint(logParts["message"])
 			h, p := parseMessage(msg)
-			fmt.Printf("Message:\n%s\n%s\n", h, p)
+			msgList[h].UpdateMessage(p)
 		}
 	}(channel)
 
