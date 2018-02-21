24c70951f3e27b97515c13e5e7bfdb16e479b40f
Алексей Елагин <a.elagin@corp.youdo.com>
2018-02-21 16:23:52 +0300

    fix array range bug

--- emailmessage.go
+++ emailmessage.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"log"
 	"strconv"
 	"strings"
 	"sync"
@@ -34,14 +35,12 @@ func (m *messageList) Save(key, val string) {
 	m.mtx.RUnlock()
 
 	if ok {
-		m.mtx.Lock()
 		m.Messages[key].UpdateMessage(val)
-		m.mtx.Unlock()
 	} else {
 		m.mtx.Lock()
 		m.Messages[key] = &emailMessage{}
-		m.Messages[key].UpdateMessage(val)
 		m.mtx.Unlock()
+		m.Messages[key].UpdateMessage(val)
 	}
 	if m.CheckComplete(key) {
 		exportChan <- key
@@ -80,33 +79,49 @@ func (msg *emailMessage) UpdateMessage(logRecord string) {
 	}
 	// Добавляем получателя, релей, задержку, статус отправки
 	if strings.HasPrefix(logRecord, "to=") {
-		var domain string
+		var domain, RawStatus, strStatus, StatusMsg string
+		var delay float64
 		stringParts := strings.Split(logRecord, ",")
+		partLen := len(stringParts)
 		to := strings.TrimPrefix(stringParts[0], "to=<")
-		relay := strings.TrimPrefix(stringParts[1], " relay=")
-		domainList := strings.Split(strings.Split(relay, "[")[0], ".")
-		switch domainLen := len(domainList); domainLen {
-		case 2:
-			domain = domainList[0] + "." + domainList[1]
-		case 1:
-			domain = domainList[0]
-		default:
-			domain = domainList[len(domainList)-2] + "." + domainList[len(domainList)-1]
+		if partLen > 4 {
+			relay := strings.TrimPrefix(stringParts[1], " relay=")
+			domainList := strings.Split(strings.Split(relay, "[")[0], ".")
+			switch domainLen := len(domainList); domainLen {
+			case 2:
+				domain = domainList[0] + "." + domainList[1]
+			case 1:
+				domain = domainList[0]
+			default:
+				domain = domainList[len(domainList)-2] + "." + domainList[len(domainList)-1]
+			}
+			RawStatus = stringParts[5]
+			fullStatus := strings.SplitN(RawStatus, "=", 2)
+			if len(fullStatus) > 1 {
+				strStatus = fullStatus[1]
+			} else {
+				log.Printf("Error log part: %v\n", fullStatus)
+			}
+			splitStatuses := strings.SplitN(strStatus, " ", 2)
+			if len(splitStatuses) > 1 {
+				StatusMsg = splitStatuses[1]
+			} else {
+				StatusMsg = "Unknown"
+				log.Printf("Error log part: %v\n", splitStatuses)
+			}
+			delay, _ = strconv.ParseFloat(strings.Split(stringParts[2], "=")[1], 32)
+		} else {
+			log.Printf("Error log record: %s\n", logRecord)
 		}
-		RawStatus := stringParts[5]
-		fullStatus := strings.SplitN(RawStatus, "=", 2)[1]
-		splitStatuses := strings.SplitN(fullStatus, " ", 2)
-		delay, _ := strconv.ParseFloat(strings.Split(stringParts[2], "=")[1], 32)
-
 		msg.mtx.Lock()
 		msg.To = strings.TrimSuffix(to, ">")
 		msg.Relay = strings.ToLower(domain)
 		msg.Delay = delay
-		msg.StatusCode = splitStatuses[0]
-		msg.StatusMsg = splitStatuses[1]
+		msg.StatusCode = StatusMsg
 		msg.rawString = append(msg.rawString, logRecord)
 		msg.mtx.Unlock()
 		return
+
 	}
 	msg.mtx.Lock()
 	msg.UpdateTime = int32(time.Now().Unix())
