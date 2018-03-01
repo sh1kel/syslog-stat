a2a9dc0249c060b756f35dce43e5ae42bbd76c54
Алексей Елагин <a.elagin@corp.youdo.com>
2018-03-01 17:39:39 +0300

    причесал код

--- main.go
+++ main.go
@@ -6,7 +6,6 @@ import (
 	nativesyslog "log/syslog"
 	"net/http"
 	"os"
-	"runtime"
 	"strings"
 	"sync"
 	"time"
@@ -67,7 +66,7 @@ func parseMessage(msg string) (ok bool, header, payload string) {
 
 func init() {
 	var err error
-	runtime.GOMAXPROCS(16)
+	// runtime.GOMAXPROCS(16)
 	// категория, в которую пишутся payment логи (/etc/rsyslog.d/50-default.conf)
 	logwriter, err = nativesyslog.New(nativesyslog.LOG_LOCAL4, "syslog-go")
 	if err == nil {
@@ -81,43 +80,26 @@ func main() {
 	cleanChan := make(chan string, 100)
 	exportChan := make(chan *emailMessage, 100)
 	avgcountCh := make(chan *domainDelay, 100)
-	controlChan := make(chan struct{}, 3)
+	controlChan := make(chan struct{}, 1)
 	sigChan := make(chan os.Signal, 1)
-	signal.Notify(sigChan, os.Interrupt, syscall.SIGTERM)
+	ticker := time.NewTicker(1 * time.Minute)
 	var wg sync.WaitGroup
 
-	// profiling
-	/*
-		f, err := os.Create("cpu.prof")
-		if err != nil {
-			log.Fatal("could not create CPU profile: ", err)
-		}
-		if err := pprof.StartCPUProfile(f); err != nil {
-			log.Fatal("could not start CPU profile: ", err)
-		}
-	*/
-	// end
-
-	ticker := time.NewTicker(1 * time.Minute)
-
-	handler := syslog.NewChannelHandler(syslogChannel)
+	// перехватываем ctl+c, SIGKILL
+	signal.Notify(sigChan, os.Interrupt, syscall.SIGTERM)
 	domainDelays := DelayInit()
 
+	handler := syslog.NewChannelHandler(syslogChannel)
 	server := syslog.NewServer()
 	server.SetFormat(syslog.RFC5424)
 	server.SetHandler(handler)
 	server.ListenUDP("127.0.0.1:5141")
 	server.Boot()
-	/*
-		go func () {
-			log.Println(http.ListenAndServe("localhost:6060", nil))
-		}()
-	*/
+
 	http.HandleFunc("/stat", webStat)
 	http.HandleFunc("/delays/", domainDelays.avgDelay)
 	http.HandleFunc("/domains", domainDelays.listDomains)
 	http.HandleFunc("/debug", domainDelays.webDebug)
-
 	go http.ListenAndServe("127.0.0.1:8081", nil)
 
 	wg.Add(4)
@@ -136,8 +118,6 @@ func main() {
 	wg.Wait()
 
 	server.Wait()
-
 	ticker.Stop()
 	logwriter.Info("Server stopped. " + strconv.Itoa(int(time.Now().Unix())))
-
 }
